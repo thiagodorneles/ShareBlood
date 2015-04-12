2a99c3d97086c18619615a5b0e925bd78f1edef7
Thiago Dorneles <thiagodornelesrs@gmail.com>
2015-04-12 10:45:15 -0300

    Tratamento melhorado para Authorization do header HTTP

--- assets/js/factories/apiService.js
+++ assets/js/factories/apiService.js
@@ -38,21 +38,12 @@ angular
         };
 
         api.post = function(url, parameters, include_header) {
-            // var header = {};
-            // if (include_header) {
-                // console.log(include_header);
-                // header = {
-                //     'Authorization' : 'Token token=' + userService.get_token()
-                // };
-                $http.defaults.headers.common.Authorization = 'Token token=' + userService.get_token();
-            // }
+            $http.defaults.headers.common.Authorization = 'Token token=' + userService.get_token();
             return $http.post(base_url + url, parameters);
         };
 
         api.put = function(url, parameters, include_header) {
-            // if (include_header) {
-                $http.defaults.headers.common.Authorization = 'Token token=' + userService.get_token();
-            // }
+            $http.defaults.headers.common.Authorization = 'Token token=' + userService.get_token();
             return $http.put(base_url + url, parameters);
         };
 
